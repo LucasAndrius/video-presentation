c05e477afd12925c8bea103ac77cd3f95bf0160e
Lucas <lucasandrios@gmail.com>
2023-10-27 22:28:18 -0300

    add switch options

--- components/VideoByDay/index.tsx
+++ components/VideoByDay/index.tsx
@@ -8,7 +8,23 @@ export const VideoByDay = () => {
     let videoToShow = '';
 
     switch(dayOfWeek) {
-        
+        case 0:
+            videoToShow = 'assets/videos/domingo.mp4';
+            break;
+        case 1: videoToShow = 'assets/videos/segundafeira.mp4';
+            break;
+        case 2: videoToShow = 'assets/videos/tercafeira.mp4';
+            break;
+        case 3: videoToShow = 'assets/videos/quartafeira.mp4';
+            break;
+        case 4: videoToShow = 'assets/videos/quintafeira.mp4';
+            break;
+        case 5: videoToShow = 'assets/videos/sextafeira.mp4';
+            break;
+        case 6: videoToShow = 'assets/videos/sabado.mp4';
+            break;
+        case 7: videoToShow = 'assets/videos/domingo.mp4'
+            break;
         default:
             videoToShow = 'assets/videos/default.mp4'
     }
